803fcc723793ec8cc8d1e0248ae0f39c498e5b72
omerfarukkzl <omerfarukkizil@outlook.com>
2026-02-09 15:34:06 +0300

    refactor: use environment variable for tags API base URL

--- src/frontend/contacts-web/src/app/core/services/tags-api.service.ts
+++ src/frontend/contacts-web/src/app/core/services/tags-api.service.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Tag } from '../models/tag.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TagsApiService {
   private readonly http = inject(HttpClient);
-  private readonly baseUrl = '/api/tags';
+  private readonly baseUrl = `${environment.apiUrl}/tags`;
 
   getTags(): Observable<Tag[]> {
     return this.http.get<Tag[]>(this.baseUrl);
